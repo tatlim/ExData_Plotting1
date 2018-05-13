e325a8eeae35242db41f581daabf6e8cd22983e1
Stefan Schmidt <stefan7schmidt@gmx.net>
2018-05-14 00:28:40 +0200

    power vs datetime plot.

---
+++ plot2.R
@@ -0,0 +1,22 @@
+# read in data file
+hpc_all <-
+  read.table(
+    "household_power_consumption.txt",
+    header = TRUE,
+    sep = ";",
+    na.strings = "?",
+    stringsAsFactors = FALSE
+  )
+
+# subset to dates: 2007-02-01 and 2007-02-02
+hpc <- subset(hpc_all, Date == "1/2/2007" | Date == "2/2/2007")
+
+# convert date and time
+hpc$datetime <- strptime(paste(hpc$Date, hpc$Time), "%d/%m/%Y %H:%M:%S")
+
+# plot power vs. datetime
+with(hpc, plot(datetime, Global_active_power, xlab = "", ylab = "Global Active Power (kilowatts)", type = "l"))
+
+# copy from screen device to png file
+dev.copy(png, "plot2.png", width = 480, height = 480)
+dev.off()
